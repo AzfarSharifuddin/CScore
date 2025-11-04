aa00e865739897abbda76868958b284724d5d62f
Azfar Sharifuddin <147719131+AzfarSharifuddin@users.noreply.github.com>
2025-11-05 02:46:45 +0800

    Update forum

--- cscore/lib/ForumModule/forum.dart
+++ cscore/lib/ForumModule/forum.dart
@@ -22,55 +22,7 @@ class _ForumState extends State<Forum> {
         backgroundColor: Colors.grey[200],
       ),
       body: Text('Forum page'),
-      bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            currentIndex: 3,
-            selectedItemColor: Colors.blue,
-            unselectedItemColor: Colors.black,
-            onTap: (index){
-              if(index == 0){
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Dashboard()),);
-              }
-              else if(index == 1){
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Learning()),);
-              }
-              else if(index == 2){
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Quiz()),);
-              }
-              else if(index == 4){
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const UserProfile()),);
-              }
-            },
-            items: const[
-              BottomNavigationBarItem(
-                    
-                    icon: Icon(Icons.home),
-                    label: '', 
-                    
-                ),
-                BottomNavigationBarItem(
-                    
-                    icon: Icon(Icons.school),
-                    label: '', 
-                    
-                ),
-                BottomNavigationBarItem(
       
-                    icon: Icon(Icons.lightbulb_circle_outlined),
-                    label: '', 
-                ),
-                BottomNavigationBarItem(
-                    
-                    icon: Icon(Icons.forum),
-                    label: '',
-                ),
-                BottomNavigationBarItem(
-
-                    icon: Icon(Icons.person),
-                    label: '',
-                ),
-            ],
-        ),
     );
   }
 }
