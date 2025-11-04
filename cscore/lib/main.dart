92509132723924b5318d4df1858055ce1d2a9101
Muhammad Thaqif Ammar <147963330+thaqifammar89@users.noreply.github.com>
2025-11-05 04:30:46 +0800

    Reverted main.dart

--- cscore/lib/main.dart
+++ cscore/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cscore/DashboardModule/dashboard.dart';
 import 'package:cscore/ForumModule/forum.dart';
 import 'package:cscore/LearningModule/learning.dart';
 import 'package:cscore/QuizModule/quiz.dart';
+import 'package:cscore/AccountModule/screen/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cscore/firebase_options.dart';
